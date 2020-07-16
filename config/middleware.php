7d09f4238a230bce2b8b421b855db1405100cacf
Markus Claus <m.claus@ynfinite.de>
2020-07-16 17:01:39 +0200

    slim config

---
+++ config/middleware.php
@@ -0,0 +1,29 @@
+<?php
+
+use Slim\App;
+use Slim\Middleware\ErrorMiddleware;
+use Slim\Views\TwigMiddleware;
+use Slim\Middleware\Session;
+
+return function (App $app) {
+    // Parse json, form data and xml
+    $app->addBodyParsingMiddleware();
+
+    $app->getRouteCollector()->setCacheFile(
+        getcwd().'/tmp/cache/routes.cache'
+    );
+    
+    // Add the Slim built-in routing middleware
+    $app->addRoutingMiddleware();
+
+    $app->add(TwigMiddleware::createFromContainer($app));
+
+    $app->add(new Session([
+        'name' => 'ynfinite-session',
+        'autorefresh' => true,
+        'lifetime' => '1 hour'
+    ]));
+
+    // Catch exceptions and errors
+    $app->add(ErrorMiddleware::class);
+};
---
+++ config/ynfinite.php
@@ -0,0 +1,61 @@
+<?php
+
+$dotenv = Dotenv\Dotenv::create(getcwd());
+$dotenv->load();
+
+$ynfinite_server = 'https://live-server.ynfinite.de';
+$ynfinite_port = 443;
+$fileservice_server = 'https://live-files.ynfinite.de';
+$fileservice_port = 443;
+if (getenv('DEV')) {
+    $ynfinite_server = 'https://ynfinite-node';
+    $ynfinite_port = 4242;
+    $fileservice_server = 'http://imageservice';
+    $fileservice_port = 3333;
+}
+
+
+return [
+    'installPassword' => getenv('YN_INSTALL_PASSWORD'),
+    'pageTypes' => array("html", "htm"),
+    'services' => [
+        'frontend' => [
+            'host' => $ynfinite_server,
+            'port' => $ynfinite_port,
+            'controller' => "/v1/cms/newfrontend/p/",
+            'gdprInfo' => "/v1/cms/newfrontend/gdpr/info",
+            'use_frontend_cache' => false,
+            'frontend_cache_url' => getenv('YNFINITE_FRONTEND_CACHE_URL'),
+            'frontend_cache_port' => getenv('YNFINITE_FRONTEND_CACHE_PORT')
+        ],
+        'form' => [
+            'host' => $ynfinite_server,
+            'port' => $ynfinite_port,
+            'controller' => "/v1/cms/newfrontend/p/form/",
+            'gdpr' => "/v1/cms/newfrontend/gdpr/request",
+            'gdprUpdate' => "/v1/cms/newfrontend/gdpr/update",
+            'gdprDelete' => "/v1/cms/newfrontend/gdpr/delete"
+        ],
+        'sitemap' => [
+            'host' => $ynfinite_server,
+            'port' => $ynfinite_port,
+            'controller' => "/v1/cms/newfrontend/p/sitemap"
+        ],
+        'robotsTxt' => [
+            'host' => $ynfinite_server,
+            'port' => $ynfinite_port,
+            'controller' => "/v1/cms/newfrontend/p/robotstxt"
+        ],
+        'file' => [
+            'host' => $fileservice_server,
+            'port' => $fileservice_port
+        ]
+    ],
+    "templateDir" => "web/templates",
+    "settings" => [
+        'api_key' => getenv('YN_API_KEY'),
+        'service_id' => getenv('YN_SERVICE_ID'),
+        'dev' => getenv('DEV'),
+        'static_pages' => getenv('STATIC_PAGES'),
+    ]
+];
