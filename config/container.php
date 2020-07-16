c6809d808c8e65d8024c497105b27f6cf3e33cde
Markus Claus <m.claus@ynfinite.de>
2020-07-16 18:32:09 +0200

    Action RenderPage with everything in the request domain

--- config/container.php
+++ config/container.php
@@ -4,6 +4,8 @@ use Psr\Container\ContainerInterface;
 use Slim\App;
 use Slim\Factory\AppFactory;
 use Slim\Middleware\ErrorMiddleware;
+use SlimSession\Helper;
+use Slim\Views\Twig;
 
 return [
     'settings' => function () {
@@ -14,6 +16,20 @@ return [
         AppFactory::setContainer($container);
 
         return AppFactory::create();
+    },
+
+    Helper::class => function (ContainerInterface $container) {
+        return new Helper;
+    },
+
+    Twig::class => function (ContainerInterface $container) {
+        return Twig::create(getcwd() . '/src/templates',
+            [
+                'cache' => getcwd() . '/tmp/cache',
+                'auto_reload' => true,
+                'debug' => false,
+            ]
+        );
     },
 
     ErrorMiddleware::class => function (ContainerInterface $container) {
