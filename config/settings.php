e1ada6601521de51a48fb08f3fb8c6df78385c69
Mario Fahlandt <m.fahlandt@koenigspunkt.de>
2020-07-17 09:56:47 +0200

    Disabled middleware view

--- config/settings.php
+++ config/settings.php
@@ -2,7 +2,7 @@
 
 // Error reporting for production
 error_reporting(0);
-ini_set('display_errors', '0');
+ini_set('display_errors', '1');
 
 // Timezone
 date_default_timezone_set('Europe/Berlin');
