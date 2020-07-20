904d9cd22f2acfcd5368a4a5d65553f8f8855e4a
Markus Claus <m.claus@ynfinite.de>
2020-07-20 11:35:55 +0200

    Errorhandling unfinished

---
+++ src/Exception/YnfiniteException.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Exception;
+
+/* @TODO: Seperate errors from API and errors from PHP into two seperate Exception classes using interfaces */
+
+class YnfiniteException extends \Exception
+{
+    private $templates;
+    private $data;
+    private $redirect = null;
+    private $renderType = "error";
+
+    public function __construct($message, $code = 0, $pipeBody = false, \Exception $previous = null)
+    {
+        $finalMessage = "";
+
+            var_dump($message);
+
+        if (is_array($message["message"]) && $message["message"]["templates"] && $message["message"]["data"]) {
+            $this->renderType = "template";
+            $this->data = $message["message"]["data"];
+            $this->templates = $message["message"]["templates"];
+        }
+        if (is_array($message["message"]) && $message["message"]["redirect"]) {
+            $this->renderType = "template";
+            $this->redirect = $message["message"]["redirect"];
+        }
+
+        $finalMessage = $this->__toString($message);
+
+        parent::__construct($finalMessage, $code, $previous);
+    }
+
+
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    public function getTemplates()
+    {
+        return $this->templates;
+    }
+
+    public function getRenderType()
+    {
+        return $this->renderType;
+    }
+
+    public function getRedirect()
+    {
+        return $this->redirect;
+    }
+
+    public function __toString()
+    {
+        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+    }
+
+}
