470e8cd8414a7ae4de736384b7b37cbff6f89e1e
Markus Claus <m.claus@ynfinite.de>
2020-07-23 17:43:51 +0200

    Fixed static page generation when a db entry exists

--- src/Domain/Request/Repository/RequestCacheRepository.php
+++ src/Domain/Request/Repository/RequestCacheRepository.php
@@ -18,7 +18,18 @@ class RequestCacheRepository {
             'filename' => $filename,
         ];
 
-        return (int) $this->connection->table('static_page_cache')->updateOrInsert($values);
+        $cache = $this->connection->table('static_page_cache')->where("cache_key", "=", $cacheKey)->first();
+
+        if($cache->id) {
+            var_dump("UPDATING");
+            return (int) $this->connection->table('static_page_cache')->where("cache_key", "=", $cacheKey)->update($values);
+        }
+        else {
+            var_dump("INSERTING");
+            return (int) $this->connection->table('static_page_cache')->insert($values);
+        }
+
+        
     }
 
     public function invalidateCache($cacheKey) {
