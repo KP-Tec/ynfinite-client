afda5661b3f18baa6653e4159a4b3a6441e4bd58
Markus Claus <m.claus@ynfinite.de>
2020-07-23 15:00:49 +0200

    Consent manager can now be reopened to change consents. Hide consent manager flag from pages is considered

--- config/routes.php
+++ config/routes.php
@@ -17,6 +17,7 @@ return function (App $app) {
 
     $app->get('/sitemap.xml', \App\Action\GetSitemapAction::class);
     $app->get('/robots.txt', \App\Action\GetRobotsTxtAction::class);
+    $app->get('/yn-updater/update', \App\Action\UpdateToVersionAction::class);
     $app->get('/yn-cache/invalidate', \App\Action\InvalidateCacheAction::class);
     $app->get('[/{params:.*}]', \App\Action\RenderPageAction::class);
     $app->post('[/{params:.*}]', \App\Action\SendFormAction::class);
