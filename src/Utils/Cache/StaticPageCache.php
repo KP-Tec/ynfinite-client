c8597cb1dd26f393c17faae3a49e5b5d8463ce84
Mario Fahlandt <m.fahlandt@koenigspunkt.de>
2020-07-31 21:56:43 +0200

    Add Cache for Listing & language cache

--- src/Utils/Cache/StaticPageCache.php
+++ src/Utils/Cache/StaticPageCache.php
@@ -2,60 +2,101 @@
 
 namespace App\Utils\Cache;
 
-class StaticPageCache {
+class StaticPageCache
+{
     const BASIC_PATH = "/../tmp/static_pages/";
 
-    private static function createCacheName() {
-        $url = $_SERVER["REQUEST_URI"];
-        $break = explode('/', $url);
-        $file = $break[count($break) - 1];
+    private static function createCacheName()
+    {
+        $additional_keys = '';
+        if ($_SERVER['QUERY_STRING']) {
 
-        if($file) {
-            $cachefile = 'cached-'.substr_replace($file ,"", -5);
+            parse_str($_SERVER['QUERY_STRING'], $parsedQuery);
+            $hasOtherParams = false;
+            foreach ($parsedQuery as $key => $value) {
+                if ($key !== '_y__ynfinitePerPage' && $key !== '_y__ynfinitePage' && $key !== '_y__ynfiniteForm') {
+                    $hasOtherParams = true;
+                }
+                if ($key === '_y__ynfinitePerPage') {
+                    $additional_keys .= '_y__ynfinitePerPage_' . $parsedQuery['_y__ynfinitePerPage'];
+                }
+                if ($key === '_y__ynfinitePage' ) {
+                    $additional_keys .= '_y__ynfinitePage_' . $parsedQuery['_y__ynfinitePage'];
+                }
+                if ('_y__ynfiniteForm') {
+                    $additional_keys .= '_y__ynfiniteForm_' . $parsedQuery['_y__ynfiniteForm'];
+                }
+            }
+            if($hasOtherParams){
+                $additional_keys = "NOT_CACHED_LISTING_FILTER";
+            }
         }
-        else {
+
+        $url = strtok($_SERVER["REQUEST_URI"], '?');
+        $file = str_replace('/', '-', $url);
+        $file = str_replace('.html', '', $file);
+
+        if ($file[0] === "-") {
+            $file = substr($file, 1);
+        }
+
+        if ($file) {
+            $cachefile = 'cached-' . $file;
+        } else {
             $cachefile = "cached-index";
         }
 
-        if($_COOKIE["ynfinite-cookies"]) {
+        if ($_COOKIE["ynfinite-cookies"]) {
             $ynCookie = json_decode($_COOKIE["ynfinite-cookies"]);
             $activeScripts = implode("-", $ynCookie->activeScripts);
-            if($activeScripts) $cachefile .= "-".$activeScripts;
+            if ($activeScripts) $cachefile .= "-" . $activeScripts;
         }
 
-
-
-        $cachefile .= '.html';
-
+        $cachefile .= '.html' . $additional_keys;
 
         return $cachefile;
     }
 
-    public function createStaticPage($content, $pageType) {
-        if($pageType !== "listing" && $pageType !== "404") {
-            $filename = StaticPageCache::createCacheName();
-            file_put_contents(getcwd().StaticPageCache::BASIC_PATH.$filename, $content);
-            return $filename;
+
+    public function createStaticPage($content, $pageType)
+    {
+
+        if ($pageType === "listing" && $_SERVER['QUERY_STRING']) {
+            parse_str($_SERVER['QUERY_STRING'], $parsedQuery);
+            $hasOtherParams = false;
+            foreach ($parsedQuery as $key => $value) {
+                if ($key !== '_y__ynfinitePerPage' && $key !== '_y__ynfinitePage' && $key !== '_y__ynfiniteForm') {
+                    $hasOtherParams = true;
+                }
+            }
+            if ($hasOtherParams) {
+                return false;
+            }
         }
-        return false;
+        if ($pageType === "404") {
+            return false;
+        }
+        $filename = StaticPageCache::createCacheName();
+        file_put_contents(getcwd() . StaticPageCache::BASIC_PATH . $filename, $content);
+        return $filename;
     }
 
 
-    public static function getCachedPage() {
+    public static function getCachedPage()
+    {
         $filename = StaticPageCache::createCacheName();
-        
-        $path = getcwd().StaticPageCache::BASIC_PATH.$filename;
-        
-        if(file_exists($path)) {
+        $path = getcwd() . StaticPageCache::BASIC_PATH . $filename;
+        if (file_exists($path)) {
             return file_get_contents($path);
         }
         return false;
     }
 
-    public static function invalidateCache($filename) {
-        $path = getcwd().StaticPageCache::BASIC_PATH.$filename;
+    public static function invalidateCache($filename)
+    {
+        $path = getcwd() . StaticPageCache::BASIC_PATH . $filename;
 
         $result = unlink($path);
-        return $result;    
+        return $result;
     }
 }
