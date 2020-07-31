e9f7cdbea63d8b5aebf498d89683129d9c83bcd8
Markus Claus <m.claus@ynfinite.de>
2020-07-31 08:51:04 +0200

    Consents are now required when then are mandatory in the Ynfinite backend

--- config/routes.php
+++ config/routes.php
@@ -3,23 +3,10 @@
 use Slim\App;
 
 return function (App $app) {
-    
-    /*
-    $app->group('/ynfinite', function (RouteCollectorProxy $group) {
-        $group->get('/install', Install::class . ":index")->setName('install');
-        $group->post('/install', Install::class . ":save")->setName('install_save');
-        $group->get('/install/password', Install::class . ":password")->setName('password_protected');
-        $group->post('/install/password/check', Install::class . ":checkPassword")->setName('password_check');
-        $group->get('/install/success', Install::class . ":success")->setName('install_save');
-
-    });
-    */
-
     $app->get('/sitemap.xml', \App\Action\GetSitemapAction::class);
     $app->get('/robots.txt', \App\Action\GetRobotsTxtAction::class);
     $app->get('/yn-updater/update', \App\Action\UpdateToVersionAction::class);
     $app->get('/yn-cache/invalidate', \App\Action\InvalidateCacheAction::class);
     $app->get('[/{params:.*}]', \App\Action\RenderPageAction::class);
     $app->post('[/{params:.*}]', \App\Action\SendFormAction::class);
-    
 };
