4900a54b5e2ab210c14dee8f9a1e1be7526b5fa4
Markus Claus <m.claus@ynfinite.de>
2020-08-07 18:52:35 +0200

    Invalidate all. Batch cache invalidation

--- config/routes.php
+++ config/routes.php
@@ -7,6 +7,7 @@ return function (App $app) {
     $app->get('/robots.txt', \App\Action\GetRobotsTxtAction::class);
     $app->get('/yn-updater/update', \App\Action\UpdateToVersionAction::class);
     $app->get('/yn-cache/invalidate', \App\Action\InvalidateCacheAction::class);
+    $app->get('/yn-cache/invalidateAll', \App\Action\InvalidateAllCacheAction::class);
     $app->get('[/{params:.*}]', \App\Action\RenderPageAction::class);
     $app->post('[/{params:.*}]', \App\Action\SendFormAction::class);
 };
