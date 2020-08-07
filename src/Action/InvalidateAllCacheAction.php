8ef600cca4ba478fb880d512291f395e0768d317
Markus Claus <m.claus@ynfinite.de>
2020-08-07 18:52:35 +0200

    Invalidate all. Batch cache invalidation

---
+++ src/Action/InvalidateAllCacheAction.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Action;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+use App\Domain\Request\Repository\RequestCacheRepository;
+
+use SlimSession\Helper as SessionHelper;
+
+final class InvalidateAllCacheAction
+{
+    public function __construct(RequestCacheRepository $repository) {
+        $this->repository = $repository;
+    }
+
+    public function __invoke(
+        ServerRequestInterface $request, 
+        ResponseInterface $response
+    ): ResponseInterface {
+        $result = $this->repository->invalidateAllCache();
+ 
+        $response->getBody()->write((string)json_encode(array("success" => $result)));
+
+        return $response
+            ->withHeader('Content-Type', 'application/json')
+            ->withStatus(200);
+        
+    }
+}
