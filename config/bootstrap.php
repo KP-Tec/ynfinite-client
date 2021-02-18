d2fe6e74640c62ededdcd835389c6728ac5417a9
Markus Claus <m.claus@ynfinite.de>
2021-02-18 12:56:58 +0100

    Disable APCU in .env

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -3,6 +3,14 @@
 use DI\ContainerBuilder;
 use Slim\App;
 
+function toBoolean($value) {
+    if(!$value || $value === "false") return false;
+    return true;
+}
+
+$dotenv = Dotenv\Dotenv::create(__DIR__ . "/../");
+$dotenv->load();
+
 $containerBuilder = new ContainerBuilder();
 
 // Set up settings
@@ -10,7 +18,10 @@ $containerBuilder->addDefinitions(__DIR__ . '/container.php');
 
 // Build PHP-DI Container instance
 $containerBuilder->enableCompilation(__DIR__ . '/../tmp/cache');
-$containerBuilder->enableDefinitionCache();
+if(toBoolean(getenv("ENABLE_APCU"))) {
+    $containerBuilder->enableDefinitionCache();
+}
+
 $container = $containerBuilder->build();
 
 // Create App instance
