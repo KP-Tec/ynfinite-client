7831d7a82ef3ab1af47d90bd3a02a83ca1ed34db
Moritz Limanski <moritz@limanski.de>
2021-08-18 11:08:11 +0200

    fix cookie-settings bug

--- src/assets/js/cookies.js
+++ src/assets/js/cookies.js
@@ -15,12 +15,16 @@ const YnfiniteCookies = {
             this.ynSetDefaultCookieSettings();
           });
 
-        document
-          .getElementById("yn-cookies__show-configuration")
-          .addEventListener("click", (e) => {
-            e.preventDefault();
-            this.ynCookiesShowPage("configuration");
-          });
+
+
+		if (document.getElementById("yn-cookies__show-configuration")) {
+			document
+				.getElementById("yn-cookies__show-configuration")
+				.addEventListener("click", (e) => {
+				e.preventDefault();
+				this.ynCookiesShowPage("configuration");
+			});
+		}
 
         document
           .getElementById("yn-cookies__show-information")
