526e8f80c024698c880e1a8ad38119bcef3b77b5
Leon Müller <l.mueller@ynfinite.com>
2022-04-26 12:06:58 +0200

    improved design & layout

--- src/assets/js/cookies.js
+++ src/assets/js/cookies.js
@@ -1,174 +1,155 @@
 const YnfiniteCookies = {
-  setup() {
-    document.addEventListener("DOMContentLoaded", () => {
-      const manager = document.getElementById("yn-cookies");
+	setup() {
+		document.addEventListener('DOMContentLoaded', () => {
+			const manager = document.getElementById('yn-cookies')
 
-      if (manager) {
-        if (manager.dataset.hideManager !== "true") {
-          this.ynCheckForCookieConsents();
-        }
+			if (manager) {
+				if (manager.dataset.hideManager !== 'true') {
+					this.ynCheckForCookieConsents()
+				}
 
-        document
-          .getElementById("yn-cookies__allow-all")
-          ?.addEventListener("click", (e) => {
-            e.preventDefault();
-            this.ynSetDefaultCookieSettings();
-          });
+				document.getElementById('yn-cookies__allow-all')?.addEventListener('click', (e) => {
+					e.preventDefault()
+					this.ynSetDefaultCookieSettings()
+				})
 
-        document
-          .getElementById("yn-cookies__show-configuration")
-          ?.addEventListener("click", (e) => {
-            e.preventDefault();
-            this.ynCookiesShowPage("configuration");
-          });
+				document.getElementById('yn-cookies__deny-all')?.addEventListener('click', (e) => {
+					e.preventDefault()
+					this.ynDenyAllCookieSettings()
+				})
 
-        document
-          .getElementById("yn-cookies__show-information")
-          ?.addEventListener("click", (e) => {
-            e.preventDefault();
-            this.ynCookiesShowPage("information");
-          });
+				document.getElementById('yn-cookies__show-configuration')?.addEventListener('click', (e) => {
+					e.preventDefault()
+					this.ynCookiesShowPage('configuration')
+				})
 
-        document
-          .getElementById("yn-cookies__set-cookies")
-          ?.addEventListener("click", (e) => {
-            e.preventDefault();
-            this.ynSetCookieSettings();
-          });
+				document.getElementById('yn-cookies__show-information')?.addEventListener('click', (e) => {
+					e.preventDefault()
+					this.ynCookiesShowPage('information')
+				})
 
-        const changeSelectionButton = document.getElementById(
-          "yn-cookies__change-selection"
-        );
+				document.getElementById('yn-cookies__set-cookies')?.addEventListener('click', (e) => {
+					e.preventDefault()
+					this.ynSetCookieSettings()
+				})
 
-        changeSelectionButton &&
-          changeSelectionButton.addEventListener("click", (e) => {
-            e.preventDefault();
-            this.ynCookiesShowPage("configuration");
-            this.showCookieConsent(true);
-          });
+				const changeSelectionButton = document.getElementById('yn-cookies__change-selection')
 
-        const consentButtons = document.querySelectorAll(
-          ".yn-cookie-consent--okay"
-        );
+				changeSelectionButton &&
+					changeSelectionButton.addEventListener('click', (e) => {
+						e.preventDefault()
+						this.ynCookiesShowPage('configuration')
+						this.showCookieConsent(true)
+					})
 
-        for (let i = 0; i < consentButtons.length; i++) {
-          let button = consentButtons[i];
-          button.addEventListener("click", (e) => {
-            e.preventDefault();
-            const id = button.dataset.consentId;
-            this.ynAcceptCookie(id);
-          });
-        }
-      }
-    });
-  },
+				const consentButtons = document.querySelectorAll('.yn-cookie-consent--okay')
 
-  showCookieConsent(hideBackButton = false) {
-    const e = document.getElementById("yn-cookies");
-    e && e.classList.add("yn-cookies--show");
+				for (let i = 0; i < consentButtons.length; i++) {
+					let button = consentButtons[i]
+					button.addEventListener('click', (e) => {
+						e.preventDefault()
+						const id = button.dataset.consentId
+						this.ynAcceptCookie(id)
+					})
+				}
+			}
+		})
+	},
 
-    if (hideBackButton) {
-      document.getElementById("yn-cookies__show-information").style.display =
-        "none";
-    } else {
-      document.getElementById("yn-cookies__show-information").style.display =
-        "block";
-    }
-  },
+	showCookieConsent(hideBackButton = false) {
+		const e = document.getElementById('yn-cookies')
+		e && e.classList.add('yn-cookies--show')
 
-  hideCookieConsent() {
-    const e = document.getElementById("yn-cookies");
-    e && e.classList.remove("yn-cookies--show");
-  },
+		if (hideBackButton) {
+			document.getElementById('yn-cookies__show-information').style.display = 'none'
+		} else {
+			document.getElementById('yn-cookies__show-information').style.display = 'block'
+		}
+	},
 
-  ynCookiesShowPage(e) {
-    const t = document.querySelectorAll("[data-yn-cookie-page]");
-    for (let e = 0; e < t.length; e++)
-      t[e].classList.remove("yn-cookies__page--visible"),
-        t[e].classList.add("yn-cookies__page--hidden");
-    const n = document.querySelector(`[data-yn-cookie-page="${e}"]`);
-    n &&
-      (n.classList.remove("yn-cookies__page--hidden"),
-      n.classList.add("yn-cookies__page--visible"));
-  },
+	hideCookieConsent() {
+		const e = document.getElementById('yn-cookies')
+		e && e.classList.remove('yn-cookies--show')
+	},
 
-  ynSetCookie(e, t, n) {
-    const o = new Date();
-    o.setTime(o.getTime() + 24 * n * 60 * 60 * 1e3);
-    const i = `expires=${o.toUTCString()}`;
-    document.cookie = `${e}=${t};${i};path=/`;
-  },
+	ynCookiesShowPage(e) {
+		const t = document.querySelectorAll('[data-yn-cookie-page]')
+		for (let e = 0; e < t.length; e++) t[e].classList.remove('yn-cookies__page--visible'), t[e].classList.add('yn-cookies__page--hidden')
+		const n = document.querySelector(`[data-yn-cookie-page="${e}"]`)
+		n && (n.classList.remove('yn-cookies__page--hidden'), n.classList.add('yn-cookies__page--visible'))
+	},
 
-  ynGetCookie(e) {
-    const t = `${e}=`,
-      n = decodeURIComponent(document.cookie).split(";");
-    for (let e = 0; e < n.length; e++) {
-      let o = n[e];
-      for (; " " === o.charAt(0); ) o = o.substring(1);
-      if (0 === o.indexOf(t)) return o.substring(t.length, o.length);
-    }
-    return "";
-  },
+	ynSetCookie(e, t, n) {
+		const o = new Date()
+		o.setTime(o.getTime() + 24 * n * 60 * 60 * 1e3)
+		const i = `expires=${o.toUTCString()}`
+		document.cookie = `${e}=${t};${i};path=/`
+	},
 
-  addScripts(e, t) {
-    for (let n = 0; n < t.length; n++) {
-      if (!document.querySelector(`script[src="${t[n]}"]`)) {
-        const o = document.createElement("script");
-        (o.src = t[n]), e.appendChild(o);
-      }
-    }
-  },
+	ynGetCookie(e) {
+		const t = `${e}=`,
+			n = decodeURIComponent(document.cookie).split(';')
+		for (let e = 0; e < n.length; e++) {
+			let o = n[e]
+			for (; ' ' === o.charAt(0); ) o = o.substring(1)
+			if (0 === o.indexOf(t)) return o.substring(t.length, o.length)
+		}
+		return ''
+	},
 
-  ynCheckForCookieConsents() {
-    const e = this.ynGetCookie("ynfinite-cookies");
-    if (e) {
-      !0 === JSON.parse(e).done
-        ? this.hideCookieConsent()
-        : this.showCookieConsent();
-    } else this.showCookieConsent();
-  },
+	addScripts(e, t) {
+		for (let n = 0; n < t.length; n++) {
+			if (!document.querySelector(`script[src="${t[n]}"]`)) {
+				const o = document.createElement('script')
+				;(o.src = t[n]), e.appendChild(o)
+			}
+		}
+	},
 
-  ynSetCookieSettings() {
-    const e = document.querySelector("#yn-cookies-form"),
-      t = {};
-    if (e) {
-      const n = new FormData(e);
-      t.activeScripts = n.getAll("activatedScripts[]");
-    }
-    (t.done = !0),
-      this.ynSetCookie("ynfinite-cookies", JSON.stringify(t), 365),
-      window.location.reload();
-  },
+	ynCheckForCookieConsents() {
+		const e = this.ynGetCookie('ynfinite-cookies')
+		if (e) {
+			!0 === JSON.parse(e).done ? this.hideCookieConsent() : this.showCookieConsent()
+		} else this.showCookieConsent()
+	},
 
-  ynSetDefaultCookieSettings() {
-    const e = document.querySelectorAll("[data-yn-cookie-settings-id] input"),
-      t = { done: false, activeScripts: [] };
+	ynSetCookieSettings() {
+		const e = document.querySelector('#yn-cookies-form'),
+			t = {}
+		if (e) {
+			const n = new FormData(e)
+			t.activeScripts = n.getAll('activatedScripts[]')
+		}
+		;(t.done = !0), this.ynSetCookie('ynfinite-cookies', JSON.stringify(t), 365), window.location.reload()
+	},
 
-    if (e.length > 0) {
-      const n = [];
-      for (let t = 0; t < e.length; t += 1) n.push(e[t].value);
-      t.activeScripts = n;
-    }
-    (t.done = !0),
-      this.ynSetCookie("ynfinite-cookies", JSON.stringify(t), 365),
-      window.location.reload();
-  },
+	ynSetDefaultCookieSettings() {
+		const e = document.querySelectorAll('[data-yn-cookie-settings-id] input'),
+			t = { done: false, activeScripts: [] }
 
-  ynAcceptCookie(e) {
-    let t = this.ynGetCookie("ynfinite-cookies");
-    -1 === (t = JSON.parse(t)).activeScripts.findIndex((t) => t === e) &&
-      t.activeScripts.push(e),
-      this.ynSetCookie("ynfinite-cookies", JSON.stringify(t), 365),
-      window.location.reload();
-  },
+		if (e.length > 0) {
+			const n = []
+			for (let t = 0; t < e.length; t += 1) n.push(e[t].value)
+			t.activeScripts = n
+		}
+		;(t.done = !0), this.ynSetCookie('ynfinite-cookies', JSON.stringify(t), 365), window.location.reload()
+	},
 
-  ynDeclineCookie(e) {
-    let t = this.ynGetCookie("ynfinite-cookies");
-    const n = (t = JSON.parse(t)).activeScripts.findIndex((t) => t === e);
-    n > -1 && t.activeScripts.splice(n, 1),
-      this.ynSetCookie("ynfinite-cookies", JSON.stringify(t), 365),
-      window.location.reload();
-  },
-};
+	ynDenyAllCookieSettings() {
+		const t = { done: true, activeScripts: [] }
+		this.ynSetCookie('ynfinite-cookies', JSON.stringify(t), 365), window.location.reload()
+	},
+
+	ynAcceptCookie(e) {
+		let t = this.ynGetCookie('ynfinite-cookies')
+		;-1 === (t = JSON.parse(t)).activeScripts.findIndex((t) => t === e) && t.activeScripts.push(e), this.ynSetCookie('ynfinite-cookies', JSON.stringify(t), 365), window.location.reload()
+	},
+
+	ynDeclineCookie(e) {
+		let t = this.ynGetCookie('ynfinite-cookies')
+		const n = (t = JSON.parse(t)).activeScripts.findIndex((t) => t === e)
+		n > -1 && t.activeScripts.splice(n, 1), this.ynSetCookie('ynfinite-cookies', JSON.stringify(t), 365), window.location.reload()
+	},
+}
 
 export default YnfiniteCookies
