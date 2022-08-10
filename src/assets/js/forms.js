9a737f72e8f5516efd56a2dbba23250b5badf9e1
Leon Müller <l.mueller@ynfinite.com>
2022-08-10 12:23:27 +0200

    improved form loading animation

--- src/assets/js/forms.js
+++ src/assets/js/forms.js
@@ -45,7 +45,7 @@ const YnfiniteForms = {
 		sendButton.style.width = sendButton.offsetWidth + 'px'
 		sendButton.style.textAlign = 'center'
 		sendButton.style.opacity = 0.5
-		sendButton.style.cursor = 'none'
+		sendButton.style.cursor = 'inherit'
 		sendButton.disabled = true;
 
     const loading = setInterval(() => {
