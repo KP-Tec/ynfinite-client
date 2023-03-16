1c7979ac6beb4d458ea45d1bb3629b6ae0dc5319
Leon-2212 <l.mueller@ynfinite.com>
2023-03-16 14:20:42 +0100

    render

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,14 +3,14 @@ import './js/general'
 
 import YnfiniteConsents from './js/cookies'
 import YnfiniteForms from './js/forms'
-import YnfiniteBotProtection from "./js/botprotection"
+import YnfiniteBotProtection from './js/botprotection'
 
 YnfiniteConsents.setup()
 YnfiniteForms.setup()
 YnfiniteBotProtection.setup()
 
 window.$_yn = {
-	forms: { 
+	forms: {
 		updateUrl: YnfiniteForms.updateUrl,
 		repopulate: YnfiniteForms.repopulateForm,
 		enable: YnfiniteForms.enableForm,
