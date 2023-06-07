ca6ecd77311239616c7a8de487e998f6de3a7fef
Leon Müller <l.mueller@ynfinite.com>
2023-06-07 14:00:42 +0200

    fixed date field & cookie manager bugs

--- src/assets/dates.js
+++ src/assets/dates.js
@@ -1,4 +1,6 @@
 import flatpickr from './node_modules/flatpickr/dist/flatpickr.min.js'
+import weekSelect from './node_modules/flatpickr/dist/plugins/weekSelect/weekSelect.js'
+import monthSelect from './node_modules/flatpickr/dist/plugins/monthSelect/index.js'
 import { German } from './node_modules/flatpickr/dist/l10n/de.js'
 
 flatpickr.localize(German)
@@ -10,3 +12,6 @@ flatpickr.setDefaults({
 	clickOpens: true,
 	mode: 'single',
 })
+
+window.weekSelect = weekSelect
+window.monthSelect = monthSelect
