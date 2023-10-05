85dfddc789ff48d351c2c7a07869d42efa4ef43a
Leon Müller <l.mueller@ynfinite.com>
2023-10-05 10:28:43 +0200

    fixes for parameter

--- src/Utils/Twig/TwigUtils.php
+++ src/Utils/Twig/TwigUtils.php
@@ -477,16 +477,17 @@ class TwigUtils
         if($link){
             return $this->twig->render(
                 $this->getTemplate("link:link"),
-                ["link" => $link, "classes" => $options['classes'] = "", "params" => $options['params'] = ""]
+                ["link" => $link, "classes" => $options['classes'] ?? "", "params" => $options['params'] ?? ""]
             );
         }
     }
 
     public function links($context, $links, $options = []) {
+        var_dump($options['classes']);
         if($links){
             return $this->twig->render(
                 $this->getTemplate("links:links"),
-                ["links" => $links, "classes" => $options['classes'] = "", "params" => $options['params'] = ""]
+                ["links" => $links, "classes" => $options['classes'] ?? "", "params" => $options['params'] ?? ""]
             );
         }
     }
@@ -495,7 +496,7 @@ class TwigUtils
         if($accordions){
             return $this->twig->render(
                 $this->getTemplate("accordions:accordions"),
-                ["accordions" => $accordions, "classes" => $options['classes'] = "", "params" => $options['params'] = ""]
+                ["accordions" => $accordions, "classes" => $options['classes'] ?? "", "params" => $options['params'] ?? ""]
             );
         }
     }
@@ -553,14 +554,14 @@ class TwigUtils
     public function article($context, $article, $options = []) {
         return $this->twig->render($this->getTemplate('article:article'), [
             'article' => $article,
-            'imgConfig' => $options['imgConfig'] = "",
+            'imgConfig' => $options['imgConfig'] ?? "",
         ]);
     }
 
     public function articleComponent($context, $component, $options = []) {
         return $this->twig->render(
             $this->getTemplate('article:' . $component['type']),
-            ['component' => $component, 'imgConfig' => $options['imgConfig'] = ""]
+            ['component' => $component, 'imgConfig' => $options['imgConfig'] ?? ""]
         );
     }
 
