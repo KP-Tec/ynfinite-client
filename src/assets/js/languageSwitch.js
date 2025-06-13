f6e68650b2f5b1c05278140b7487ba35abf19790
Leon Müller <l.mueller@ynfinite.com>
2025-06-13 11:15:17 +0200

    fixed translation popup loading before site is loaded

--- src/assets/js/languageSwitch.js
+++ src/assets/js/languageSwitch.js
@@ -1,5 +1,14 @@
 const YnfiniteLanguageSwitch = {
 	setup() {
+		// Wait for DOM to be ready
+		if (document.readyState === 'loading') {
+			document.addEventListener('DOMContentLoaded', () => this.init())
+		} else {
+			this.init()
+		}
+	},
+
+	init() {
 		const languageSwitchWrapper = document.querySelector('.yn-languageSwitch')
 
 		if (!languageSwitchWrapper) return
